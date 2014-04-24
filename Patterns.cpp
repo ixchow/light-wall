299081f80736378c9d85b11757404b256518b762
Jim McCann (retchow) <ix@tchow.com>
2014-04-24 19:42:40 -0400

    eliminate the -- now ported -- circle code

--- Patterns.cpp
+++ Patterns.cpp
@@ -64,29 +64,7 @@ public:
 	uint8_t grid[LedsX * LedsY];
 };
 
-/*
-//TODO: port to new class-based patterns:
-void Grid_Circle_Init(PatternState *s) {
-	for (int16_t y = 0; y < (int16_t)LedsY; ++y) {
-		for (int16_t x = 0; x < (int16_t)LedsX; ++x) {
-			int16_t dis = (2 * x + 1 - LedsX) * (2 * x + 1 - LedsX) * PerJugY * PerJugY
-			            + (2 * y + 1 - LedsY) * (2 * y + 1 - LedsY) * PerJugX * PerJugX;
-			s->grid[y * LedsX + x] = dis / 8;
-		}
-	}
-}
-
 //----------------------------------------------------
-
-void Grid_Read(PatternState *s, uint16_t x, uint16_t y, Px *px) {
-	uint8_t v = s->grid[y * LedsX + x];
-	v += s->t * (s->p1 & 0x7);
-	if (s->p1 & 0x8) v = 255 - v;
-	read_ramp(s->ramp1, v, px);
-}
-
-//----------------------------------------------------
-*/
 
 template< typename P >
 Pattern *create() { return new P(); }
